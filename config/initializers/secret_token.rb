796761aa4b4ee7daddef234bb266deaaecdd028f
Adam <adam.miribyan@gmail.com>
2011-03-09 06:25:14 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bournemouth::Application.config.secret_token = '7b90acd5b701016e9983fa45d5f65f282bfa137496b877064a935caaeb1cdeacfce743bb1bbb8d12c8929cef4bf4c7149fefb44da7cd94c6868f26e79f95f828'
